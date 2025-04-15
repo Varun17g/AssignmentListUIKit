f5b93bc59cea5f05870ce46c51e4191b1aa1b3c6
Varun Bhati <varunbhati@Varuns-MacBook-Pro.local>
2025-04-15 18:42:17 +0530

    Initial commit

---
+++ AssignmentUIKit/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AssignmentUIKit
+//
+//  Created by Varun Bhati on 15/04/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ AssignmentUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentUIKit
+//
+//  Created by Varun Bhati on 15/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
